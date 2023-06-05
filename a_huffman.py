1728196512b80e02e6fdeb7aaab554e5269e9a68
Khoa Nguyen <115177830+khoawack@users.noreply.github.com>
2023-06-04 22:24:29 -0700

    Add files via upload
    Done

--- a_huffman.py
+++ a_huffman.py
@@ -4,6 +4,7 @@
 
 # the input, what we want to encode
 message: str = 'Hello there'
+message = message.upper()
 
 # the output, should be all 0's and 1s
 result: str = str()
@@ -20,63 +21,82 @@ coding: dict = dict()   # key  -> a letter
                         # item -> a binary encoding
 
 
-# STEP 0 - TODO
+# STEP 0
 ## defining our data structures
-class Node: # NOT given to students
-    # TODO
-    
-    def __init__(self):
-        return
+class Node:  # NOT given to students
+    def __init__(self, weight, letter):
+        self.weight = weight
+        self.letter = letter
+        self.left = None
+        self.right = None
 
 ## defining operations
 ### recursively traverses the huffman tree to record the codes
-def retrieve_codes(v: Node, path: str=''):
+def retrieve_codes(v: Node, path: str = ''):
     global coding
-    if v.letter != None: # if 'TODO': # TODO
-        coding[v.letter] = None # TODO
+    if v.letter is not None:
+        coding[v.letter] = path
     else:
-        retrieve_codes(None, None) # TODO
-        retrieve_codes(None, None) # TODO
+        retrieve_codes(v.left, path + '0')
+        retrieve_codes(v.right, path + '1')
+
 
 # STEP 1
 ## counting the frequencies - TODO
+for i in message:
+    if i not in freq:
+        freq[i] = 1
+    else:
+        freq[i] += 1
 
 
 # STEP 2
 ## initialize the nodes - TODO
-nodes = list()
-nodes.append(Node(0, 'a'))
+nodes = []
+for letter, frequency in freq.items():
+    nodes.append(Node(frequency, letter))
 
-# STEP 3 - TODO
-## combine each nodes until there's only one item in the nodes list
+
+# STEP 3 
+## combine nodes until there's only one item in the nodes list
 while len(nodes) > 1:
     ## sort based on weight
     nodes.sort(key=lambda x: x.weight, reverse=True)
 
     ## get the first min
-    min_a: Node = nodes.pop()
+    min_a = nodes.pop()
 
     ## get the second min
-    min_b: Node = nodes.pop()
+    min_b = nodes.pop()
 
     ## combine the two
-    combined: Node = None # TODO
+    combined = Node(min_a.weight + min_b.weight, None)
+    combined.left = min_a
+    combined.right = min_b
 
     ## put the combined nodes back in the list of nodes
     nodes.append(combined)
+
 
 # STEP 4
 ## reconstruct the codes
 huff_root = nodes[0]
 retrieve_codes(huff_root)
-result: str = str() # TODO (hint coding[letter] -> code)
+result = ''
+for letter in message:
+    result += coding[letter]
+
 
 # STEP 5
-## analyize compression performance
-n_original_bits: int = len(message) * 8
-n_encoded_bits: int = len(result)
-compression_ratio: float = (1 - n_encoded_bits / n_original_bits) * 100
+## analyze compression performance
+n_original_bits = len(message) * 8
+n_encoded_bits = len(result)
+compression_ratio = (1 - n_encoded_bits / n_original_bits) * 100
 
+print(freq)
+print(message)
+print(coding)
+print(result)
 print(f'original: {n_original_bits:^4d} bits')
 print(f'encoded : {n_encoded_bits:^4d} bits')
 print(f'savings : {int(compression_ratio):^4d} % compression')
--- b_huffman-analysis.py
+++ b_huffman-analysis.py
@@ -5,14 +5,30 @@
 import matplotlib.pyplot as plt
 
 # DATA - lyrics
-POKEMON_LYRICS = 'I wanna be the very best. Like no one ever was. To catch them is my real test. To train them is my cause. I will travel across the land. Searching far and wide. Each Pokemon to understand. The power that\'s inside. (Pokemon, gotta catch \'em all.) Its you and me. I know it\'s my destiny. (Pokemon.) Oh, you\'re my best friend. In a world we must defend. (Pokemon, gotta catch \'em all.) A heart so true. Our courage will pull us through. You teach me and I\'ll teach you. Pokemon. (gotta catch \'em all.) Gotta catch \'em all. Yeah. Every challenge along the way. With courage I will face. I will battle every day. To claim my rightful place. Come with me, the time is right. There\'s no better team. Arm in arm we\'ll win the fight. It\'s always been our dream. (Pokemon, gotta catch \'em all.) Its you and me. I know it\'s my destiny (Pokemon.) Oh, you\'re my best friend. In a world we must defend. (Pokemon, gotta catch \'em all.) A heart so true. Our courage will pull us through. You teach me and I\'ll teach you. Pokemon (gotta catch \'em all.) Gotta catch \'em all. Gotta catch \'em all. Gotta catch \'em all. Gotta catch \'em all. Yeah! (Pokemon, gotta catch \'em all). Its you and me. I know it\'s my destiny. (Pokemon) Oh, you\'re my best friend. In a world we must defend. (Pokemon, gotta catch \'em all.) A heart so true. Our courage will pull us through. You teach me and I\'ll teach you Pokemon. (gotta catch \'em all). Gotta catch \'em all. (Pokemon)'
-JIGGLE_JIGGLE = 'You have to have something that sticks. You have to have something that\'s monumental. When you walk out on stage, that\'s been monumental. (Jiggle, jiggle) Can you remember any of the rap that you did? My money don\'t jiggle, jiggle, it folds. I like to see you wiggle, wiggle, for sure. It makes me want to dribble, dribble, you know. Riding in my Fiat, you really have to see it. Six feet two in a compact, no slack. But luckily the seats go back. I got a knack to relax in my mind. Sipping some red, red wine. I sip booze from chalices, holding my palaces. Crib is so crampy suckers suffer from paralysis. Rhymes, I write them in the castle. You try to diss me and pretty soon your ass. Will squat in a cell \'cause I can tell you it\'s illegal. Treason, that\'s the reason I\'m regal. You do the time for the crime of lèse-majesté. And **** the police \'cause they can\'t arrest me. (They can\'t arrest me, they can\'t arrest me). (I like to see you wiggle, it makes me dribble, fancy a fiddle?). My money don\'t jiggle, jiggle, it folds. I like to see you wiggle, wiggle, for sure. It makes me want to dribble, dribble, you know. Riding in my Fiat, you really have to see it. Six feet two in a compact, no slack. But luckily the seats go back. I got a knack to relax in my mind. Sipping some red, red wine. (I like to see you wiggle, it makes me dribble, fancy a fiddle?). (I like to see you wiggle, it makes me dribble, fancy a fiddle?). (I like to see you wiggle, it makes me dribble, fancy a fiddle?). (I like to see you wiggle, it makes me dribble, fancy a fiddle?)'
-ALPHABET = 'Now it\'s time for our wrap up. Let\'s give it everything we\'ve got. Ready, begin. Artificial amateurs aren\'t at all amazing. Analytically, I assault, animate things. Broken barriers bounded by the bomb beat. Buildings are broken, basically I\'m bombarding. Casually create catastrophes, casualties. Canceling cats got their canopies collapsing. Detonate a dime of dank daily doin\' dough. Demonstrations, Don Dada on the down low. Eatin\' other editors with each and every energetic. Epileptic episode, elevated etiquette. Furious fat fabulous fantastic. Flurries of funk felt feeding the fanatics. Imitators idolize, I intimidate. In a instant, I\'ll rise in a irate state. Juiced on my jams like jheri curls, jockin\' joints. Justly, it\'s just me, writin\' my journals. Kindly I\'m kindling all kinds of ink on. Karate kick type Brits in my kingdom. Let me live a long life, lyrically lessons is. Learned lame louses just lose to my livery. My mind makes marvelous moves, masses. Marvel and move, many mock what I\'ve mastered.  Niggas nap knowin\' I\'m nice naturally. Knack, never lack, make noise nationally.  Operation, opposition, off, not optional. Out of sight, out of mind, wide beaming opticals. Perfected poem, powerful punchlines. Pummeling petty powder puffs in my prime. Quite quaint quotes keep quiet it\'s Quannum Quarrelers ain\'t got a quarter of what we got, uh. Really raw raps, risin\' up rapidly. Riding the rushing radioactivity. Super scientifical sound search sought. Silencing super fire saps that are soft. Tales ten times talented, they\'re too tough. Take that, challengers, get a tune up. Universal, unique untouched. Unadulterated, the raw uncut. Verb vice Lord victorious valid. Violate vibes that are vain make \'em vanished. Why I\'m all well, would a wise wordsmith. Just weaving up words weeded up, on my work shift. Xerox, my X-ray-diation holes extra large. X-height letters and xylophone tones.'
+# Never Gonna Give You Up
+POKEMON_LYRICS = "We\'re no strangers to love You know the rules and so do I (do I) A full commitment\'s what I\'m thinking of You wouldn\'t get this from any other guy I just wanna tell you how I\'m feeling Gotta make you understand Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you We\'ve known each other for so long Your heart\'s been aching, but you\'re too shy to say it (say it) Inside, we both know what\'s been going on (going on) We know the game and we\'re gonna play it And if you ask me how I\'m feeling Don\'t tell me you\'re too blind to see Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you We\'ve known each other for so long Your heart\'s been aching, but you\'re too shy to say it (to say it) Inside, we both know what\'s been going on (going on) We know the game and we\'re gonna play it I just wanna tell you how I\'m feeling Gotta make you understand Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you I did not use the tutoring option because my friend offered to help me out. She did a great job, but I think I struggled during the midterm because there was only 50 minutes and I was unable to solve the problems fast enough. I\'m planning to enroll in the course again during the fall. Is there anything else I need to do to be eligible for grade forgiveness? Thank you for your time and consideration."
+# You've Got a Friend in Me
+JIGGLE_JIGGLE = "You\'ve got a friend in me You\'ve got a friend in me When the road looks rough ahead And you\'re miles and miles From your nice warm bed You just remember what your old pal said Boy, you\'ve got a friend in me Yeah, you\'ve got a friend in me You\'ve got a friend in me You\'ve got a friend in me You got troubles, I\'ve got \'em too There isn\'t anything I wouldn\'t do for you We stick together and see it through \'Cause you\'ve got a friend in me You\'ve got a friend in me Some other folks might be A little bit smarter than I am Bigger and stronger too Maybe But none of them will ever love you The way I do It\'s me and you, boy And as the years go by Our friendship will never die You\'re gonna see it\'s our destiny You\'ve got a friend in me You\'ve got a friend in me You\'ve got a friend in me"
+# Sunflower
+ALPHABET = 'Ayy, ayy, ayy, ayy (ooh) Ooh, ooh, ooh, ooh (ooh) Ayy, ayy Ooh, ooh, ooh, ooh Needless to say, I keep her in check She was a bad-bad, nevertheless (yeah) Callin\' it quits now, baby, I\'m a wreck (wreck) Crash at my place, baby, you\'re a wreck (wreck) Needless to say, I\'m keeping her in check She was all bad-bad, nevertheless Callin\' it quits now, baby, I\'m a wreck Crash at my place, baby, you\'re a wreck Thinkin\' in a bad way, losin\' your grip Screamin\' at my face, baby, don\'t trip Someone took a big L, don\'t know how that felt Lookin\' at you sideways, party on tilt Ooh-ooh-ooh Some things you just can\'t refuse She wanna ride me like a cruise And I\'m not tryna lose Then you\'re left in the dust Unless I stuck by ya You\'re the sunflower I think your love would be too much Or you\'ll be left in the dust Unless I stuck by ya You\'re the sunflower You\'re the sunflower Every time I\'m leavin\' on you (ooh) You don\'t make it easy, no (no, no) Wish I could be there for you Give me a reason to, oh (oh) Every time I\'m walkin\' out I can hear you tellin\' me to turn around Fightin\' for my trust and you won\'t back down Even if we gotta risk it all right now, oh (now) I know you\'re scared of the unknown (\'known) You don\'t wanna be alone (alone) I know I always come and go (and go) But it\'s out of my control And you\'ll be left in the dust Unless I stuck by ya You\'re the sunflower I think your love would be too much Or you\'ll be left in the dust Unless I stuck by ya You\'re the sunflower You\'re the sunflower (yeah)'
+
 
 # DATA - mantras
-GREEN_LATTERN = 'In brightest day, in blackest night, No evil shall escape my sight. Let those who worship evil\'s might, Beware my power... Green Lantern\'s light!'
-JEDI_CODE = 'Emotion, yet peace. Ignorance, yet knowledge. Passion, yet serenity. Chaos, yet harmony. Death, yet the Force.'
-SITH_CODE = 'Peace is a lie. There is only Passion. Through Passion, I gain Strength. Through Strength, I gain Power. Through Power, I gain Victory. Through Victory my chains are Broken. The Force shall free me.'
+#Team Rocket
+GREEN_LATTERN = "Prepare for trouble! And make it double! To protect the world from devastation! To unite all peoples within our nation! To denounce the evils of truth and love! To extend our reach to the stars above! Jessie! James! Team Rocket blasts off at the speed of light! Surrender now, or prepare to fight! Meowth! That\'s right!"
+#Spiderman
+JEDI_CODE = 'with great power there must also come – great responsibility'
+#Ratatouille
+SITH_CODE = "anyone can cook"
+
+# STEP 0 - TODO
+## defining our data structures
+class Node:  # NOT given to students
+    def __init__(self, weight, letter):
+        self.weight = weight
+        self.letter = letter
+        self.left = None
+        self.right = None
 
 # the input, what we want to encode
 def huffman(message:str) -> float:
@@ -32,22 +48,61 @@ def huffman(message:str) -> float:
     coding: dict = dict()   # key  -> a letter
                             # item -> a binary encoding
 
+    def retrieve_codes(v: Node, path: str = ''):
+        if v.letter is not None:
+            coding[v.letter] = path
+        else:
+            retrieve_codes(v.left, path + '0')
+            retrieve_codes(v.right, path + '1')
 
-    # STEP 0
-    ## defining our data structures
-    ## defining operations
 
     # STEP 1
-    ## counting the frequencies
+    ## counting the frequencies - TODO
+    for i in message:
+        if i not in freq:
+            freq[i] = 1
+        else:
+            freq[i] += 1
+
 
     # STEP 2
-    ## initialize the nodes
+    ## initialize the nodes - TODO
+    nodes = []
+    for letter, frequency in freq.items():
+        nodes.append(Node(frequency, letter))
 
-    # STEP 3
-    ## combine each nodes until there's only one item in the nodes list
+    print(len(nodes))
+
+
+# STEP 3 
+## combine nodes until there's only one item in the nodes list
+    while len(nodes) > 1:
+        ## sort based on weight
+        nodes.sort(key=lambda x: x.weight, reverse=True)
+
+        ## get the first min
+        min_a = nodes.pop()
+
+        ## get the second min
+        min_b = nodes.pop()
+
+        ## combine the two
+        combined = Node(min_a.weight + min_b.weight, None)
+        combined.left = min_a
+        combined.right = min_b
+
+        ## put the combined nodes back in the list of nodes
+        nodes.append(combined)
+
 
     # STEP 4
     ## reconstruct the codes
+    huff_root = nodes[0]
+    retrieve_codes(huff_root)
+    result = ''
+    for letter in message:
+        result += coding[letter]
+
 
     # STEP 5
     ## analyize compression performance
@@ -57,36 +112,57 @@ def huffman(message:str) -> float:
 
     return result, coding, compression_ratio
 
+
 # LYRICS
+
 plt.subplot(2, 1, 1)
-plt.suptitle('Lab 7 - Stapleton Analyzing Huffman')
+plt.ylabel("Compression %")
+
+
 
 MAX_N: int = int(128 * 3 / 2)
 
 # PLOT 1
-## POKEMON
+## Rick Roll
 ratios: list = list()
 for i in range(1, MAX_N):
     sub_message = POKEMON_LYRICS[0:i]
     _, _, ratio = huffman(sub_message)
     ratios.append(ratio)
 
-## JIGGLE JIGGLE
+
+plt.plot(ratios,linestyle='-.' , label = "Rick Roll (n=25)")
+
+
+## YGAFIM
 ratios: list = list()
 for i in range(1, MAX_N):
     sub_message = JIGGLE_JIGGLE[0:i]
     _, _, ratio = huffman(sub_message)
     ratios.append(ratio)
 
-## ALPHABET
+plt.plot(ratios,linestyle='-.', label = "You've Got a Friend in Me (n=26)")
+
+## Sunflower
 ratios: list = list()
 for i in range(1, MAX_N):
     sub_message = ALPHABET[0:i]
     _, _, ratio = huffman(sub_message)
     ratios.append(ratio)
 
-# PLOT 2
+plt.plot(ratios,linestyle='-.', label = 'Sunflower (n=27)')
+
+plt.title("Lab 7 - Nguyen Analyzing Huffman")
+plt.xlabel("x")
+plt.legend(loc='upper right')
+
+
+
+# PLOT 2x
 plt.subplot(2, 1, 2)
+plt.ylabel("Compression %")
+plt.xlabel("length of message")
+
 
 ## SITH CODE
 ratios: list = list()
@@ -95,6 +171,8 @@ for i in range(1, MAX_N):
     _, _, ratio = huffman(sub_message)
     ratios.append(ratio)
 
+plt.plot(ratios,linestyle='-.', label="Ratatouille (n=8)")
+
 ## GREEN LATERN'S OATH
 ratios: list = list()
 for i in range(1, MAX_N):
@@ -102,9 +180,18 @@ for i in range(1, MAX_N):
     _, _, ratio = huffman(sub_message)
     ratios.append(ratio)
 
+plt.plot(ratios,linestyle='-.', label="Team Rocket (n=23)")
+
 ## JEDI CODE
 ratios: list = list()
 for i in range(1, MAX_N):
     sub_message = JEDI_CODE[0:i]
     _, _, ratio = huffman(sub_message)
     ratios.append(ratio)
+
+plt.plot(ratios,linestyle='-.', label="Spiderman (n=20)")
+
+plt.legend(loc='upper right')
+
+plt.savefig("./figs/lab7_nguyen.png")
+plt.show()
